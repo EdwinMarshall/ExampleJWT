de00d8593baf1b4a82234b80a7f38b3bfb0ee21f
Edwin Tonatiuh Lozano Estrada <edwin.lozanoe@outlook.com>
2022-02-13 22:10:18 -0600

    Removed useless project

--- ExampleJWT.API2/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
+++
@@ -1,7 +0,0 @@
-namespace ExampleJWT.API.Areas.HelpPage.ModelDescriptions
-{
-    public class CollectionModelDescription : ModelDescription
-    {
-        public ModelDescription ElementDescription { get; set; }
-    }
-}
